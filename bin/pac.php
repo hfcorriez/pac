90c58889d64d3636a0b9efb3536509ea31ca3eb0
hfcorriez <hfcorriez@gmail.com>
2017-09-03 22:12:25 +0800

    Implements new Command.

--- bin/pac.php
+++ bin/pac.php
@@ -4,21 +4,17 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 use Pac\Command;
 
-$arg_parser = new Command();
+$args = Command::parse($argv);
 
-// Add a short arguments and support enum type
-$arg_parser->add('a', array('help' => 'a long time with he happy', 'enum' => array(
-    'go' => 'do some thing with go'
-)));
-
-// Add long and short arguments
-$arg_parser->add('l', array('help' => 'long arguments', 'enum' => array('a', 'b')));
-
-// Will get the arguments you want
-$args = $arg_parser->parse();
+//$command = new Command();
+//
+//$command->addOption('no', [
+//    'alias' => 'n',
+//    'type' => 'string'
+//]);
+//
+//// Will get the arguments you want
+//$args = $command->parse($argv);
 
 var_dump($args);
 exit;
-
-// Print the help message
-echo $arg_parser->help();
